4fa44447d7995c3c495e57f8d72ab74dffa14818
Naruhiko.ARASAKI <arasakinpro@gmail.com>
2018-04-13 19:32:20 +0900

    once completed

--- panel.js
+++ panel.js
@@ -3,21 +3,27 @@ $(function(){
   // パネルの挙動
   $("[id^='panel_']").on('click', function() {
     // 誤クリック防止
-    if (confirm('OK?')) {
+    if (confirm('本当にこのパネルで良いですか？')) {
       // 画像を挿入するのに使う
       let number = parseInt($(this).text());
       let url = `img/keihin${number}.jpg`;
       let target = this;
-      $('#fullImage')
+      $('#fullimage')
         .css('background-image', `url(${url})`)
-        .css('background-size', 'cover')
-        .css('background-repeat', 'no-repeat');
+        .css('background-size', 'contain')
+        .css('background-position', 'center')
+        .css('background-color', 'white')
+        .css('background-repeat', 'no-repeat')
+        .css('display', 'block');
       $(target)
-        .css('color', 'rgba(33, 35, 84, 0.8)')
+        .css('color', 'rgba(33, 35, 84, 0.5)')
         .css('background-color', 'transparent')
         .css('background-image', `url(${url})`)
         .css('background-size', 'contain')
         .css('background-repeat', 'no-repeat');
     }
+  });
+  $('#fullimage').on('click', function(){
+    $(this).css('display', 'none');
   });
 });
